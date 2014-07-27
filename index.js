ce48350f7fbfb56db1b19c208e4042b2213e0eeb
Matt O'Hagan <mco13@my.fsu.edu>
2014-07-27 03:07:12 -0400

    added top boxes

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket){
 	console.log('a user connected');
+	io.emit('chat message', 'user connected');
 
 	socket.on('chat message', function(msg){
 		io.emit('chat message', msg);
 	});
 	socket.on('disconnect', function(){
-		console.log('user disconnected');
+		io.emit('chat message', 'user disconnected');
 	});
 });
 
