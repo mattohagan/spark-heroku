3f85e615082bc5f378c4d31158bdf64f4b6d7e06
Matt O'Hagan <mco13@my.fsu.edu>
2014-07-27 05:07:33 -0400

    added username & location

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require('http');
+var server = http.Server(app);
+var io = require('socket.io')(server);
+var request = require('request');
+
 
 app.get('/', function(req, res) {
 	res.sendfile('index.html');
@@ -13,12 +16,39 @@ io.on('connection', function(socket){
 	socket.on('chat message', function(msg){
 		io.emit('chat message', msg);
 	});
+
 	socket.on('disconnect', function(){
-		io.emit('chat message', 'user disconnected');
+		io.emit('chat message', 'a user has disconnected');
 	});
+
+
+	socket.on('location', function(apiLocation){
+            var apiQuery = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.placefinder%20where%20text%3D%22' + encodeURIComponent(apiLocation) + '%22%20and%20gflags%3D%22R%22&format=json&diagnostics=true&callback=';
+            console.log("set query");
+            request({url: apiQuery, json: true }, function(err, response, body){
+            	console.log("got results");
+            	var r = body;
+                var count = r.query.count;
+                if (count > 1) {
+                    var results = r.query.results.Result[0];
+                } else if (count == 1) {
+                    var results = r.query.results.Result;
+                } else {
+                    location = 'err';
+                    var results = '';
+                }
+
+                location = results.city;
+                if (results.statecode) { 
+                	location = location + ' ' + results.statecode;
+                }
+                console.log(location);
+                io.emit('location', location);
+            });
+    });
 });
 
 var port = Number(process.env.PORT || 5000);
-http.listen(port, function() {
+server.listen(port, function() {
    console.log("Listening on " + port);
 });
